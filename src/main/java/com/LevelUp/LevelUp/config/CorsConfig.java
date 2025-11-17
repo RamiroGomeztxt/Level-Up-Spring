0311889598186291fd24328601f3f6cac78b60c7
Ramiro <ra.gomezv@duocuc.cl>
2025-11-16 21:32:42 -0300

    implementacion de 2da guia hasta el paso 9

--- src/main/java/com/LevelUp/LevelUp/config/CorsConfig.java
+++ src/main/java/com/LevelUp/LevelUp/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:9090")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
